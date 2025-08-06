827b95694b35873503b565a6beecacb577ce1f25
ahmaddn <najmyahmadmaulana@gmail.com>
2025-08-06 17:02:31 +0700

    [fix] redirect profile

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -31,7 +31,7 @@
                         <div class="dropdown-menu dropdown-menu-end shadow-sm rounded-3 p-2" style="min-width: 160px;">
 
                             <!-- Profile -->
-                            <a href="app-profile.html" class="dropdown-item d-flex align-items-center gap-2">
+                            <a href="#" class="dropdown-item d-flex align-items-center gap-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18"
                                     height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
